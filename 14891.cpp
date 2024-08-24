f717d710e523498f17465b3c815cd486d5ec797e
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2024-08-24 17:36:36 +0900

    백준 14891번: 톱니바퀴 (구현, 시뮬레이션) 골드 5
    생각만 잘하면 쉬운데 구현할 게 많으니 머리가 과부화가 온다.
    그림 그리면 쉬운데 editor에 주석으로 정리하려니 연습이 계속 필요함

--- 14891.cpp
+++ 14891.cpp
@@ -7,92 +7,60 @@ vector<vector<int> > vec(4, vector<int>(8));
 vector<int> num;
 vector<int> dir;
 
-void rightTurn(int number, int direction) {
-    if(direction == 1) {
-        if(number < 4 && vec[number-1][2] == vec[number][6]) { 
+void clockwise(int number) {
+    int temp = vec[number-1][7];
+    for(int i=6;i>=0;i--) {
+        vec[number-1][i+1] = vec[number-1][i];
 
-        } else {
-
-        }
-    } else {
-        
     }
+    vec[number-1][0] = temp;
 }
 
-void leftTurn() {
+void anticlockwise(int number) {
+    int temp = vec[number-1][0];
+    for(int i=1;i<8;i++) {
+        vec[number-1][i-1] = vec[number-1][i];
+    }
+    vec[number-1][7] = temp;
+}
 
+void rightTurn(int number, int direction) {
+    if(number < 4 && vec[number-1][2] != vec[number][6]) {
+        rightTurn(number+1 , direction * -1);
+    }
+    if(direction == 1) clockwise(number);
+    else anticlockwise(number);
+}
+
+void leftTurn(int number, int direction) {
+    if(number-2 >= 0 && vec[number-1][6] != vec[number-2][2]){
+        leftTurn(number-1 , direction * -1);
+    }
+    if(direction == 1) clockwise(number);
+    else anticlockwise(number);
 }
 
 void func(int number, int direction) {
     if(direction == 1) { //시계방향
-    
-        if(number < 4 && vec[number-1][2] == vec[number][6]) { //오른쪽 맞닿은 극이 같을 때
-            //stop
-        } else {
-            //반시계방향으로 회전
-            func(number+1 , -1);
-            
-            //number+1 - 반시계방향으로 회전
-            int temp = vec[number][0];
-            for(int i=1;i<8;i++) {
-                vec[number][i-1] = vec[number][i];
-            }
-            vec[number][7] = temp;
+        if(number < 4 && vec[number-1][2] != vec[number][6]) {
+            rightTurn(number+1 , direction * -1);
         }
 
-        if(number-2 >= 0 && vec[number-1][6] == vec[number-2][2]) { //왼쪽 맞닿은 극이 같을 때
-            //stop
-        } else {
-            //시계방향으로 회전
-            func(number-1 , 1);
-
-            int temp = vec[number-2][7];
-            for(int i=0;i<7;i++) {
-                vec[number-2][i+1] = vec[number-2][i];
-            }
-            vec[number-2][0] = temp;
+        if(number-2 >= 0 && vec[number-1][6] != vec[number-2][2]){
+            leftTurn(number-1 , direction * -1);
         }
-
         //number - 시계방향으로 회전 11시 있던게 -> 12시
-        int temp = vec[number-1][7];
-        for(int i=0;i<7;i++) {
-            vec[number-1][i+1] = vec[number-1][i];
-        }
-        vec[number-1][0] = temp;
+        clockwise(number);
     } else if(direction == -1) { //반시계방향
-        if(number < 4 && vec[number-1][2] == vec[number][6]) { //오른쪽 맞닿은 극이 같을 때
-            //stop
-        } else {
-            //시계방향으로 회전
-            func(number+1 , 1);
-            
-            //number+1 - 시계방향으로 회전
-            int temp = vec[number][7];
-            for(int i=0;i<7;i++) {
-                vec[number][i+1] = vec[number][i];
-            }
-            vec[number][0] = temp;
+        if(number < 4 && vec[number-1][2] != vec[number][6]) {
+            rightTurn(number+1 , direction * -1);
         }
 
-        if(number-2 >= 0 && vec[number-1][6] == vec[number-2][2]) { //왼쪽 맞닿은 극이 같을 때
-            //stop
-        } else {
-            //반시계방향으로 회전
-            func(number-1 , -1);
-
-            int temp = vec[number-2][0];
-            for(int i=1;i<8;i++) {
-                vec[number-2][i-1] = vec[number-2][i];
-            }
-            vec[number-2][7] = temp;
+        if(number-2 >= 0 && vec[number-1][6] != vec[number-2][2]){
+            leftTurn(number-1 , direction * -1);
         }
-
         //number - 반시계방향으로 회전 11시 있던게 -> 12시
-        int temp = vec[number-1][0];
-        for(int i=1;i<8;i++) {
-            vec[number-1][i-1] = vec[number-1][i];
-        }
-        vec[number-1][7] = temp;
+        anticlockwise(number);
     }
 }
 
@@ -106,8 +74,11 @@ int main()
     */
 
     for(int i=0;i<4;i++) {
-        for(int j=0;j<8;j++)
-            cin >> vec[i][j];
+        string s;
+        cin >> s;
+        for(int j=0;j<8;j++) {
+            vec[i][j] = s[j] - '0';
+        }
     }
     int K;
     cin >> K;
@@ -119,16 +90,14 @@ int main()
     }
 
     for(int i=0;i<K;i++) {
-
         func(num[i], dir[i]);
-
     }
     int sum = 0;
-    for(int i=0;i<4;i++) {
-        if(vec[0][0] == 1) sum += 1;
-        if(vec[1][0] == 1) sum += 2;
-        if(vec[2][0] == 1) sum += 4;
-        if(vec[3][0] == 1) sum += 8;
-    }
+    
+    if(vec[0][0] == 1) sum += 1;
+    if(vec[1][0] == 1) sum += 2;
+    if(vec[2][0] == 1) sum += 4;
+    if(vec[3][0] == 1) sum += 8;
+    
     cout << sum;
 }
