99d388227f5c25de035254dccd3934b20ea45e99
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2025-02-13 00:37:10 +0900

    Update 회의실 배정.cc

--- 백준/Gold/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.cc
@@ -4,6 +4,13 @@
 
 using namespace std;
 
+bool cmp (pair<int, int> a, pair<int, int> b) {
+    if (a.first == b.first) {
+        return a.second < b.second;
+    }
+    return a.first < b.first;
+}
+
 int main()
 {
     ios::sync_with_stdio(false);
@@ -18,7 +25,7 @@ int main()
         cin >> A[i].second >> A[i].first; //second: 첫시간, first: 끝시간
     }
 
-    sort(A.begin(), A.end());
+    sort(A.begin(), A.end(), cmp);
     
     int end = -1;
     int count = 0;
