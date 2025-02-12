03ced895f2684941f7c81a0f5a9a657bbc0f9af8
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2025-02-12 16:29:46 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    long long a, b, c;
+    cin >> a >> b >> c;
+    
+    cout << a + b + c;
+}
