285d333a74c77b7c46185932c2fc5c32f7b995cc
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2025-02-24 01:35:50 +0900

    [Bronze II] Title: 블랙잭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.cc
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cc
@@ -3,14 +3,16 @@
 
 using namespace std;
 
-vector<int> v;
 int n, m;
+vector<int> v;
 int result = 0;
 
 void b(int start, int count, int sum) {
-    if (sum > m) return;
+    if (sum > m) {
+        return ;
+    }
 
-    if(count == 3) {
+    if (count == 3) {
         result = max(result, sum);
         return;
     }
@@ -22,25 +24,17 @@ void b(int start, int count, int sum) {
 
 int main()
 {
-    /*
-        N장의 카드 중에서 3장의 카드를 고른다.
-        고른 카드의 합은 M을 넘지 않고 M과 최대한 가깝게
-
-        브루트포스 => 노가다 => 모든 경우의 수 => 재귀
-    */
-
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
 
     cin >> n >> m;
-    v.resize(n);
 
+    v.resize(n);
     for(int i = 0; i < n; i++) {
         cin >> v[i];
     }
 
     b(0, 0, 0);
-
     cout << result;
 }
