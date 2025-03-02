a8370dfee2e60c1ced29ceca84b91300e074912a
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2025-03-03 02:17:33 +0900

    [Silver IV] Title: 문자열 집합, Time: 60 ms, Memory: 7944 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.cc
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.cc
@@ -1,44 +1,7 @@
 #include <iostream>
+#include <set>
 
 using namespace std;
-
-class Node { // 트라이 자료 구조 저장용 클래스
-public: 
-    Node* next[26]; //알파벳 26진트리
-    bool isEnd; //리프노드 표시
-    Node() : isEnd(false) {
-        fill(next, next + 26, nullptr);
-    }
-
-    ~Node() {
-        for (auto& child : next)
-            delete child;
-    }
-
-    void insert(const char* key) { //문자열 삽입 함수
-        if (*key == 0) {
-            isEnd = true;
-        } else {
-            int next_index = *key - 'a';
-            if (next[next_index] == nullptr) {
-                next[next_index] = new Node();
-            }
-            next[next_index] -> insert(key + 1);
-        }
-    }
-
-    Node* find(const char* key) { //문자열 찾기 함수
-        if (*key == 0) {
-            return this;
-        }
-        int next_index = *key - 'a';
-
-        if (next[next_index] == nullptr) {
-            return nullptr;
-        }
-        return next[next_index] -> find(key + 1);
-    }
-};
 
 int main()
 {
@@ -46,23 +9,20 @@ int main()
     cin.tie(0);
     cout.tie(0);
 
+    set<string> s;
     int n, m;
     cin >> n >> m;
-    Node* root = new Node();
 
     for(int i = 0; i < n; i++) {
-        char text[501];
-        cin >> text;
-        root -> insert(text);
+        string str;
+        cin >> str;
+        s.insert(str);
     }
-
     int count = 0;
     for(int i = 0; i < m; i++) {
-        char text[501];
-        cin >> text;
-        Node* result = root -> find(text);
-
-        if (result && result -> isEnd) {
+        string str;
+        cin >> str;
+        if(s.count(str)) {
             count++;
         }
     }
