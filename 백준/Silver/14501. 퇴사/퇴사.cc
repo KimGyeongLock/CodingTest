87a48e1172688427c0dbe4f9b2f569ecc7b5e77c
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2025-03-11 22:14:14 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+vector<int> t;
+vector<int> p;
+
+int sum = 0;
+int max_v = 0;
+
+void func(int index){
+  for(int i=index;i<t.size();i++){
+    if(i+t[i] <= t.size()) {
+      //cout << p[i] << " ";
+      sum += p[i];
+      func(i+t[i]);
+      //cout << sum << "\n";
+      if(sum > max_v) max_v = sum;
+      sum -= p[i];
+    }
+  }
+  
+}
+
+int main() {
+  int N;
+  cin >> N;
+  t.resize(N);
+  p.resize(N);
+
+  for (int i = 0; i < N; i++) {
+    cin >> t[i] >> p[i];
+  }
+
+  // for (int i = 0; i < N; i++) {
+    func(0);
+  // }
+  cout << max_v;
+    
+}
