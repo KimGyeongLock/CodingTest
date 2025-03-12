1a8808abadce385d314e10a3915d63c8c5b9c220
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2025-03-12 09:05:00 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -8,16 +8,16 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    int n;
-    cin >> n;
 
-    vector<int> v(n+1);
-    v[1] = 1;
-    v[2] = 2;
-    
-    for(int i = 3; i <= n; i++) {
-        v[i] = (v[i-1] + v[i-2])  % 10007;
+    int N;
+    cin >> N;
+
+    vector<int> A(1001);
+    A[1] = 1;
+    A[2] = 2;
+
+    for(int i = 3; i <= N; i++) {
+        A[i] = (A[i-1] + A[i-2]) % 10007;
     }
-
-    cout << v[n];
+    cout << A[N];
 }
