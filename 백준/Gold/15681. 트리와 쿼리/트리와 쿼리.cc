e4504eded9f62186c36a999cc10f5bee1acd50a2
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2025-03-21 01:33:49 +0900

    [Gold V] Title: 트리와 쿼리, Time: 56 ms, Memory: 12488 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.cc
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.cc
@@ -4,15 +4,15 @@
 using namespace std;
 
 vector<vector<int> > A;
-vector<int> subtree;
 vector<bool> visited;
+vector<int> subtree;
 
 int dfs(int node) {
     visited[node] = true;
 
     subtree[node] = 1;
     for(int next : A[node]) {
-        if (!visited[next]) {
+        if(!visited[next]){
             subtree[node] += dfs(next);
         }
     }
@@ -29,12 +29,11 @@ int main()
     cin >> N >> R >> Q;
 
     A.resize(N + 1);
-    subtree.resize(N + 1);
     visited.resize(N + 1, false);
+    subtree.resize(N + 1);
     for(int i = 0; i < N - 1; i++) {
         int u, v;
         cin >> u >> v;
-
         A[u].push_back(v);
         A[v].push_back(u);
     }
@@ -42,8 +41,8 @@ int main()
     dfs(R);
 
     for(int i = 0; i < Q; i++) {
-        int q;
-        cin >> q;
-        cout << subtree[q] << "\n";
+        int a;
+        cin >> a;
+        cout << subtree[a] << "\n";
     }
 }
