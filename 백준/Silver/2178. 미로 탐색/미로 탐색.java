910fcb674ba4af6a1563e2bd1e9fe6e1e31f2244
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2025-04-24 08:40:34 +0900

    [Silver I] Title: 미로 탐색, Time: 96 ms, Memory: 13100 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,10 +1,9 @@
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int[] dx = {0, 1, 0, -1};
-	static int[] dy = {1, 0, -1, 0};
+	static int dx[] = {1, 0, -1, 0};
+	static int dy[] = {0, 1, 0, -1};
 	static int A[][];
 	static boolean visited[][];
 	static int N, M;
@@ -16,36 +15,37 @@ public class Main {
 		A = new int[N][M];
 		visited = new boolean[N][M];
 		
-		for(int i = 0; i < N; i++) {
+		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			String line = st.nextToken(); //1011010101
+			String line = st.nextToken();
 			for(int j = 0; j < M; j++) {
 				A[i][j] = Integer.parseInt(line.substring(j, j + 1));
 			}
 		}
 		
 		BFS(0, 0);
-		System.out.println(A[N - 1][M - 1]);
+		System.out.print(A[N - 1][M - 1]);
 	}
 	private static void BFS(int i, int j) {
-		Queue<int[]> queue = new LinkedList<>();
-		queue.offer(new int[] {i, j});
 		visited[i][j] = true;
-		while(!queue.isEmpty()) {
-			int now[] = queue.poll();
-			for (int k = 0; k < 4; k++) {
+		Queue<int[]> q = new LinkedList<>();
+		q.add(new int[]{i, j});
+		
+		while(!q.isEmpty()) {
+			int[] now = q.poll();
+			for(int k = 0; k < 4; k++) {
 				int x = now[0] + dx[k];
 				int y = now[1] + dy[k];
 				if (x >= 0 && y >= 0 && x < N && y < M) {
 					if (A[x][y] != 0 && !visited[x][y]) {
 						visited[x][y] = true;
 						A[x][y] = A[now[0]][now[1]] + 1;
-						queue.add(new int[] {x, y});
+						q.add(new int[] {x, y});
 					}
 				}
 			}
 		}
+		
 	}
-	
 
 }
