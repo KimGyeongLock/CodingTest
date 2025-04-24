6025818fa0d2e055884cba2f3782c00455511195
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2025-04-24 10:11:10 +0900

    [Gold IV] Title: 이분 그래프, Time: 1124 ms, Memory: 308724 KB -BaekjoonHub

---
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.java
@@ -0,0 +1,63 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static ArrayList<Integer>[] A;
+	static int[] check;
+	static boolean visited[];
+	static boolean isEven;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int testCase = Integer.parseInt(br.readLine());
+		for(int t = 0; t < testCase; t++) {
+			String[] s = br.readLine().split(" ");
+			int V = Integer.parseInt(s[0]);
+			int E = Integer.parseInt(s[1]);
+			A = new ArrayList[V + 1];
+			visited = new boolean[V + 1];
+			check = new int[V + 1];
+			isEven = true;
+			for (int i = 1; i <= V; i++) {
+				A[i] = new ArrayList<Integer>();
+			}
+			for(int i = 0; i < E; i++) {
+				s = br.readLine().split(" ");
+				int start = Integer.parseInt(s[0]);
+				int end = Integer.parseInt(s[1]);
+				A[start].add(end);
+				A[end].add(start);
+			}
+			
+			for(int i = 1; i <= V; i++) {
+				if(isEven) {
+					DFS(i);
+				} else {
+					break;
+				}
+			}
+			
+			if (isEven) {
+				System.out.println("YES");
+			} else {
+				System.out.println("NO");
+			}
+		}
+	}
+
+	private static void DFS(int start) {
+		visited[start] = true;
+		for(int i : A[start]) {
+			if (!visited[i]) {
+				check[i] = (check[start] + 1) % 2;
+				DFS(i);
+			} else if(check[start] == check[i]) {
+					isEven = false;
+			}	
+		}
+		
+	}
+
+	
+}
