535276ec2c5d4f7d6db86efb3ff5a32114533ad3
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2025-04-26 13:32:44 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 156 ms, Memory: 16468 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -11,46 +11,45 @@ public class Main {
 		
 		int time[] = new int[MAX];
 		Arrays.fill(time, Integer.MAX_VALUE);
-		
 		PriorityQueue<Node> pq = new PriorityQueue<>();
 		pq.add(new Node(N, 0));
-		time[N] = 0;
 		
 		while(!pq.isEmpty()) {
 			Node current = pq.poll();
-			int now = current.pos;
+			int nowPosition = current.pos;
 			int nowTime = current.time;
 			
-			if (now == K) {
+			if (nowPosition == K) {
 				System.out.print(nowTime);
 				return;
 			}
 			
-			if (time[now] < nowTime) continue; //현재 시간이 이미 들어가 있는 시간보다 크면 패스 -> 최소시간
+			if (nowTime > time[nowPosition]) continue;
 			
-			if (now * 2 < MAX && time[2 * now] > nowTime) {
-				time[2 * now] = nowTime;
-				pq.add(new Node(now * 2, nowTime));
+			if (nowPosition * 2 < MAX && time[nowPosition * 2] > nowTime) {
+				time[nowPosition * 2] = nowTime;
+				pq.add(new Node(nowPosition * 2, nowTime));
 			}
 			
-			if (now + 1 < MAX && time[now + 1] > nowTime + 1) {
-				time[now + 1] = nowTime + 1;
-				pq.add(new Node(now + 1, nowTime + 1));
+			if (nowPosition + 1 < MAX && time[nowPosition + 1] > nowTime + 1) {
+				time[nowPosition + 1] = nowTime + 1;
+				pq.add(new Node(nowPosition + 1, nowTime + 1));
 			}
 			
-			if (now - 1 >= 0 && time[now - 1] > nowTime + 1) {
-				time[now - 1] = nowTime + 1;
-				pq.add(new Node(now - 1, nowTime + 1));
+			if (nowPosition - 1 >= 0 && time[nowPosition - 1] > nowTime + 1) {
+				time[nowPosition - 1] = nowTime + 1;
+				pq.add(new Node(nowPosition - 1, nowTime + 1));
 			}
 		}
+		
+	
 	}
 	
-	
-	static class Node implements Comparable<Node>{
+	static public class Node implements Comparable<Node>{
 		int pos;
 		int time;
 		
-		Node(int pos, int time) {
+		public Node(int pos, int time) {
 			this.pos = pos;
 			this.time = time;
 		}
@@ -59,6 +58,8 @@ public class Main {
 		public int compareTo(Node o) {
 			return this.time - o.time;
 		}
+		
 	}
 }
+
 
