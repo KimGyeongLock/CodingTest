f69fd36b0b1d524989c1e76190fdbfa6805f5c72
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2025-05-13 01:39:26 +0900

    [level 1] Title: 모의고사, Time: 3.11 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=java) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.30 ms
+메모리: 87.7 MB, 시간: 3.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 01일 21:27:40
+2025년 05월 13일 01:39:24
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,38 +1,29 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] answers) {
-        int[] method1 = {1, 2, 3, 4, 5};
-        int[] method2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] method3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        List<Integer> list = new ArrayList<>();
+        int[] stuScore = new int[3];
+        int[][] students = {
+            {1, 2, 3, 4, 5},
+            {2, 1, 2, 3, 2, 4, 2, 5},
+            {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+        };
         
-        int one = 0;
-        for(int i = 0; i < answers.length; i++) {
-            if (answers[i] == method1[i % method1.length]) {
-                one++;
+        int maxScore = 0;
+        for(int i = 0; i < 3; i++){
+            int score = 0;
+            for(int j = 0; j < answers.length; j++) {
+                if(answers[j] == students[i][j % students[i].length]) {
+                    score++;
+                }
+                stuScore[i] = score;
             }
+            maxScore = Math.max(maxScore, score);
         }
         
-        int two = 0;
-        for(int i = 0; i < answers.length; i++) {
-            if (answers[i] == method2[i % method2.length]) {
-                two++;
-            }
+        for(int i = 0; i < 3; i++) {
+            if (maxScore == stuScore[i]) list.add(i + 1);
         }
-        
-        int three = 0;
-        for(int i = 0; i < answers.length; i++) {
-            if (answers[i] == method3[i % method3.length]) {
-                three++;
-            }
-        }
-        int max = Math.max(one, Math.max(two, three));
-        
-        ArrayList<Integer> result = new ArrayList<>();
-        if (one == max) result.add(1);
-        if (two == max) result.add(2);
-        if (three == max) result.add(3);
-    
-        
-        return result.stream().mapToInt(i -> i).toArray();
+        return list.stream().mapToInt(i -> i).toArray();
     }
 }
