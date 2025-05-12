9847796bda2a91b6d36f7814526575a09e347602
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2025-05-13 00:48:46 +0900

    [level 2] Title: H-Index, Time: 0.91 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,19 +1,15 @@
-/**
-    3 0 6 1 5
-    0 1 3 5 6
-**/
 import java.util.*;
 class Solution {
     public int solution(int[] citations) {
-        int answer = 0;
-        
         Arrays.sort(citations);
-        for(int i = 0; i < citations.length; i++) {
-            if(citations[i] >= citations.length - i) {
-                answer = Math.max(citations.length - i, answer);
+        int n = citations.length;
+        
+        for(int i = 0; i < n; i++) {
+            int h = n - i;
+            if (citations[i] >= h) {
+                return h;
             }
         }
-        
-        return answer;
+        return 0;
     }
 }
