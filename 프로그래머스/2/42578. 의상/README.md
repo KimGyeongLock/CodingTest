99a5b7e2b17483e42ae1ffebf129966c03118808
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2025-05-12 23:28:16 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=java) 
 
 ### 성능 요약
 
-메모리: 4.17 MB, 시간: 0.04 ms
+메모리: 75.2 MB, 시간: 0.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 01일 19:28:25
+2025년 05월 12일 23:28:14
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,21 +1,14 @@
-/**
-    headgear - yellow_hat, green_turban
-    eyewear - blue_sunglasses
-**/
 import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         HashMap<String, Integer> map = new HashMap<>();
-        
         for(String[] cloth : clothes) {
             map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1);
         }
-
-        int answer = 1;
-        for(int i : map.values()) {
-            answer *= (i + 1);
+        int sum = 1;
+        for(String key : map.keySet()) {
+            sum *= map.get(key) + 1;
         }
-        
-        return answer - 1;
+        return sum - 1;
     }
 }
