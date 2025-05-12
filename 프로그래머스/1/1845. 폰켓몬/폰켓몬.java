dbaa089ba6226bb14b707004bc4a2f40a628d350
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2025-05-12 23:18:01 +0900

    [level 1] Title: 폰켓몬, Time: 6.23 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,17 +1,13 @@
-/*
-    3 - 2
-    1 - 1
-    2 - 1
-    총 4개 min(2개 키의 갯수 3개)
-*/
 import java.util.*;
+
 class Solution {
     public int solution(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i : nums) {
-            map.put(i, map.getOrDefault(i, 0) + 1);
+        HashSet<Integer> set = new HashSet<>();
+        for(int i : nums) {
+            set.add(i);
         }
         
-        return Math.min(map.size(), nums.length / 2);
+        int answer = Math.min(set.size(), nums.length / 2);
+        return answer;
     }
 }
