587d4f7e7a160c1060e0339285c94a592fa7257a
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2025-08-10 00:29:14 +0900

    [level 3] Title: 네트워크, Time: 0.31 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,23 +1,21 @@
 class Solution {
-    static boolean visited[];
     public int solution(int n, int[][] computers) {
-        visited = new boolean[n];
+        boolean[] visited = new boolean[n];
         int answer = 0;
-        
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++) {
             if (!visited[i]) {
-                dfs(i, computers, n);
+                dfs(i, computers, visited, n);
                 answer++;
             }
         }
         return answer;
     }
-    private void dfs(int v, int[][] computers, int n) {
-        visited[v] = true;
-        
-        for(int i = 0; i < n; i++) {
-            if (!visited[i] && computers[v][i] == 1) {
-                dfs(i, computers, n);
+    
+    private void dfs(int start, int[][] computers, boolean[] visited, int n) {
+        visited[start] = true;
+        for(int i = 0 ; i < n; i++) {
+            if (computers[start][i] == 1 && !visited[i]) {
+                dfs(i, computers, visited, n);
             }
         }
     }
