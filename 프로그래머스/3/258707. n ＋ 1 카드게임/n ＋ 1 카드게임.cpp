7237ae726f5a22179ef663d2486f045a207d9b67
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2025-09-15 16:01:56 +0900

    [level 3] Title: n + 1 카드게임, Time: 0.03 ms, Memory: 4.06 MB -BaekjoonHub

--- 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.cpp
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.cpp
@@ -6,29 +6,32 @@ using namespace std;
 int solution(int coin, vector<int> cards) {
     int n = cards.size();
     
-    vector<char> inHand(n + 1, false), inField(n + 1, false);
-    int idx = 0; // 현재 위치 n / 3 - 1
-    for (; idx < n / 3; idx++) 
+    vector<bool> inHand(n + 1, false), inField(n + 1, false);
+    int idx = 0;
+    for (; idx < n / 3; idx++) {
         inHand[cards[idx]] = true;
+    }
     
     vector<pair<int, int>> HH;
     HH.reserve(n);
-    for (int x = 1; x <= n; x++) {
-        if (inHand[x]) {
-            int y = n + 1 - x;
-            if (inHand[y]) HH.push_back({x, y});
+    for (int i = 1; i <= n; i++) {
+        if (inHand[i]) {
+            int y = n + 1 - i;
+            if (inHand[y] && i < y) {
+                HH.push_back({i, y});
+            }
         }
     }
+    
+    vector<pair<int, int>> FF;
+    FF.reserve(n);
     vector<int> HF;
     HF.reserve(n);
-    vector<pair<int,int>> FF;
-    FF.reserve(n);
     
     auto push_field = [&](int c) {
-        if (inField[c]) return;
         int y = n + 1 - c;
-        if (inField[y]) FF.push_back({min(c, y), max(c, y)});
         if (inHand[y]) HF.push_back(y);
+        if (inField[y]) FF.push_back({c, y});
         inField[c] = true;
     };
     
@@ -40,25 +43,26 @@ int solution(int coin, vector<int> cards) {
     auto try_HH = [&]()->bool{
         while (!HH.empty()) {
             auto [a, b] = HH.back();
+            HH.pop_back();
             if (inHand[a] && inHand[b]) {
                 inHand[a] = inHand[b] = false;
                 return true;
             }
-            HH.pop_back();
-        }  
+        }
         return false;
     };
     
     auto try_HF = [&]()->bool{
         while (!HF.empty()) {
-            int h = HF.back(); HF.pop_back();
-            int c = n + 1 - h;
-            if (inHand[h] && inField[c]) {
-                inHand[h] = false;
-                inField[c] = false;
+            int a = HF.back();
+            int b = n + 1 - a;
+            HF.pop_back();
+            if (inHand[a] && inField[b]) {
+                inHand[a] = false;
+                inField[b] = false;
                 return true;
             }
-        }  
+        }
         return false;
     };
     
@@ -77,16 +81,18 @@ int solution(int coin, vector<int> cards) {
     int turn = 1;
     while (true) {
         if (idx >= n) break;
+        bool played = false;
         
         reveal();
         
-        bool played = false;
         if (try_HH()) {
             played = true;
         } else if (coin > 0 && try_HF()) {
-            coin -= 1; played = true;
+            coin -= 1;
+            played = true;
         } else if (coin >= 2 && try_FF()) {
-            coin -= 2; played = true;
+            coin -= 2;
+            played = true;
         }
         
         if (!played) break;
