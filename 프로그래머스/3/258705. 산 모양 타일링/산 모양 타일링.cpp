158a85863b7ff952c128d9b894ac47af6edc1f5b
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2025-09-16 15:46:08 +0900

    [level 3] Title: 산 모양 타일링, Time: 1.64 ms, Memory: 6.8 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.cpp
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.cpp
@@ -1,27 +1,22 @@
-#include <bits/stdc++.h>
+#include <string>
+#include <vector>
+
 using namespace std;
-
+constexpr int MOD = 10007;
 int solution(int n, vector<int> tops) {
-    const int MOD = 10007;
-
-    // A[i]: i칸까지 채웠을 때 오른쪽 경계가 '사다리꼴'
-    // B[i]: i칸까지 채웠을 때 오른쪽 경계가 '평행사변형'
-    int A = 0; // A[0]
-    int B = 1; // B[0]
-
-    for (int i = 1; i <= n; ++i) {
-        int prevA = A, prevB = B;
-
-        // A[i]
-        A = (prevA + prevB) % MOD;
-
-        // B[i]
+    vector<int> a(n+1, 0), b(n+1, 0);
+    
+    a[0] = 0;
+    b[0] = 1;
+    
+    for (int i = 1; i <= n; i++) {
         if (tops[i-1] == 1) {
-            B = ( (2*prevA) % MOD + (3*prevB) % MOD ) % MOD;
+            a[i] = (a[i-1] + b[i-1]) % MOD;
+            b[i] = (2*a[i-1] + 3*b[i-1]) % MOD;
         } else {
-            B = ( (1*prevA) % MOD + (2*prevB) % MOD ) % MOD;
+            a[i] = (a[i-1] + b[i-1]) % MOD;
+            b[i] = (a[i-1] + 2*b[i-1]) % MOD;
         }
     }
-
-    return (A + B) % MOD;
+    return (a[n] + b[n]) % 10007;
 }
