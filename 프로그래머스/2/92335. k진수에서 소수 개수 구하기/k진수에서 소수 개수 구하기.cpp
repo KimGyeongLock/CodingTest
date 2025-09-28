a6782c078b61f59e5980a965b24c417a983a51f4
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2025-09-28 20:44:34 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 8.11 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.cpp
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.cpp
@@ -19,22 +19,16 @@ vector<int> jinsu(int n, int k) {
 
 long vecToInt(vector<int>& v) {
     long res = 0;
-    for (int d : v) {
-        res = res * 10 + d;
+    for (int i = 0; i < v.size(); i++) {
+        res += v[i] * (pow(10, v.size() - i - 1));
     }
-    // for (int i = 0; i < v.size(); i++) {
-    //     res += v[i] * (pow(10, v.size() - i - 1));
-    // }
     return res;
 }
 
 bool isPrime(long x) {
     if (x < 2) return false;
-    if (x == 2 || x == 3) return true;
-    if (x % 2 == 0 || x % 3 == 0) return false;
-    
-    for (long i = 5; i * i <= x; i += 6) {
-        if (x % i == 0 || x % (i + 2) == 0) return false;
+    for (long i = 2; i * i <= x; i++) {
+        if (x % i == 0) return false;
     }
     return true;
 }
