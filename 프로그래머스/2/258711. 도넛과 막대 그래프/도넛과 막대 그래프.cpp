01187013473ec238a006fe0b4e6fd34a2604e8da
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2025-12-08 17:43:23 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 84.15 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.cpp
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.cpp
@@ -1,36 +1,27 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
 
 vector<int> solution(vector<vector<int>> edges) {
-    int MAX_ID = 0;
-    for (auto &e : edges) MAX_ID = max(MAX_ID, max(e[0], e[1]));
-
-    vector<int> indeg(MAX_ID + 1, 0), outdeg(MAX_ID + 1, 0);
-    vector<char> used(MAX_ID + 1, 0);
-
-    for (auto &e : edges) {
-        int u = e[0], v = e[1];
-        outdeg[u]++;
-        indeg[v]++;
-        used[u] = used[v] = 1;   // 그래프에 실제로 등장한 라벨만 집계
+    int max_node = 0;
+    for (auto e : edges) {
+        max_node = max(max(max_node, e[0]), e[1]);
     }
-
-    // 생성 정점 idx: indeg==0 && outdeg>=2 (등장한 노드만 대상으로)
-    int idx = -1;
-    for (int i = 0; i <= MAX_ID; ++i) {
-        if (!used[i]) continue;
-        if (indeg[i] == 0 && outdeg[i] >= 2) { idx = i; break; }
+    
+    vector<int> indegree(max_node+1, 0), outdegree(max_node+1, 0);
+    for (auto e : edges) {
+        outdegree[e[0]]++;
+        indegree[e[1]]++;
     }
-
-    int b = 0, c = 0;
-    for (int i = 0; i <= MAX_ID; ++i) {
-        if (!used[i] || i == idx) continue;
-        if (outdeg[i] == 0) b++;                         // 막대의 끝
-        if (indeg[i] >= 2 && outdeg[i] >= 2) c++;        // 8자 교차점
+    
+    int a = 0, b = 0, c = 0, d = 0;
+    for (int i = 1; i <= max_node; i++) {
+        if (indegree[i] == 0 && outdegree[i] >= 2) a = i;
+        else if (outdegree[i] == 0 && indegree[i] >= 1) c++;
+        else if (indegree[i] >= 2 && outdegree[i] >= 2) d++;
     }
-
-    int a = outdeg[idx] - b - c;                         // 도넛 개수
-    return {idx, a, b, c};
+    b = outdegree[a] - c - d;
+    return {a, b, c, d};
 }
