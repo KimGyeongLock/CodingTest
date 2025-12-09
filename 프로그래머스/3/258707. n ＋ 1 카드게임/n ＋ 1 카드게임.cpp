f1978932f1c6125d5e21940df434150235a1a0c8
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2025-12-09 18:12:27 +0900

    [level 3] Title: n + 1 카드게임, Time: 1.32 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.cpp
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.cpp
@@ -1,102 +1,88 @@
 #include <string>
 #include <vector>
+#include <unordered_set>
 
 using namespace std;
 
 int solution(int coin, vector<int> cards) {
+    int answer = 1;
     int n = cards.size();
+    int target = n + 1;
     
-    vector<bool> inHand(n + 1, false), inField(n + 1, false);
+    unordered_set<int> have;
+    unordered_set<int> pool;
+    
     int idx = 0;
-    for (; idx < n / 3; idx++) {
-        inHand[cards[idx]] = true;
+    for (; idx < n/3; idx++) {
+        have.insert(cards[idx]);
     }
     
-    vector<pair<int, int>> HH;
-    HH.reserve(n);
-    for (int i = 1; i <= n; i++) {
-        if (inHand[i]) {
-            int y = n + 1 - i;
-            if (inHand[y] && i < y) {
-                HH.push_back({i, y});
+    while(idx < n) {
+        
+        for (int k = 0; k < 2 && idx < n; k++, idx++) {
+            pool.insert(cards[idx]);
+        }
+        
+        bool progressed = false;
+        
+        int a = -1;
+        int b = -1;
+        for (int x : have) {
+            int y = target - x;
+            if (have.count(y)) {
+                a = x;
+                b = y;
+                break;
             }
         }
+        
+        if (a != -1) {
+            have.erase(a);
+            have.erase(b);
+            progressed = true;
+        }
+        
+        
+        if (!progressed && coin >= 1) {
+            a = b = -1;
+            for (int x : have) {
+                int y = target - x;
+                if (pool.count(y)) {
+                    a = x;
+                    b = y;
+                    break;
+                }
+            }
+            
+            if (a != -1) {
+                have.erase(a);
+                pool.erase(b);
+                coin -= 1;
+                progressed = true;
+            }
+        }
+        
+        if (!progressed && coin >= 2) {
+            a = b = -1;
+            for (int x : pool) {
+                int y = target - x;
+                if (pool.count(y)) {
+                    a = x;
+                    b = y;
+                    break;
+                }
+            }
+            
+            if (a != -1) {
+                pool.erase(a);
+                pool.erase(b);
+                coin -= 2;
+                progressed = true;
+            }
+        }
+        
+        if (!progressed) break;
+        answer++;
     }
-    
-    vector<pair<int, int>> FF;
-    FF.reserve(n);
-    vector<int> HF;
-    HF.reserve(n);
-    
-    auto push_field = [&](int c) {
-        int y = n + 1 - c;
-        if (inHand[y]) HF.push_back(y);
-        if (inField[y]) FF.push_back({c, y});
-        inField[c] = true;
-    };
-    
-    auto reveal = [&]() {
-        if (idx < n) push_field(cards[idx++]);
-        if (idx < n) push_field(cards[idx++]);
-    };
-    
-    auto try_HH = [&]()->bool{
-        while (!HH.empty()) {
-            auto [a, b] = HH.back();
-            HH.pop_back();
-            if (inHand[a] && inHand[b]) {
-                inHand[a] = inHand[b] = false;
-                return true;
-            }
-        }
-        return false;
-    };
-    
-    auto try_HF = [&]()->bool{
-        while (!HF.empty()) {
-            int a = HF.back();
-            int b = n + 1 - a;
-            HF.pop_back();
-            if (inHand[a] && inField[b]) {
-                inHand[a] = false;
-                inField[b] = false;
-                return true;
-            }
-        }
-        return false;
-    };
-    
-    auto try_FF = [&]()->bool{
-        while (!FF.empty()) {
-            auto [a, b] = FF.back();
-            FF.pop_back();
-            if (inField[a] && inField[b]) {
-                inField[a] = inField[b] = false;
-                return true;
-            }
-        }  
-        return false;
-    };
-    
-    int turn = 1;
-    while (true) {
-        if (idx >= n) break;
-        bool played = false;
-        
-        reveal();
-        
-        if (try_HH()) {
-            played = true;
-        } else if (coin > 0 && try_HF()) {
-            coin -= 1;
-            played = true;
-        } else if (coin >= 2 && try_FF()) {
-            coin -= 2;
-            played = true;
-        }
-        
-        if (!played) break;
-        turn++;
-    }
-    return turn;
+    return answer;
 }
