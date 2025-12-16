c8d1329be8a683c9ab3a90133d4d3d762d2c9fd0
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2025-12-16 18:21:50 +0900

    [level 2] Title: 과제 진행하기, Time: 1.31 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.cpp
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.cpp
@@ -3,10 +3,10 @@
 #include <algorithm>
 
 using namespace std;
- 
-static int toMin(const string& hhmm) {
-    int h = stoi(hhmm.substr(0, 2));
-    int m = stoi(hhmm.substr(3, 2));
+
+int toMin(string time) {
+    int h = stoi(time.substr(0, 2));
+    int m = stoi(time.substr(3, 2));
     return h * 60 + m;
 }
 
@@ -22,40 +22,40 @@ vector<string> solution(vector<vector<string>> plans) {
         v.push_back({p[0], toMin(p[1]), stoi(p[2])});
     }
     
-    sort(v.begin(), v.end(), [&](const Plan a, const Plan b) {
-       return a.start < b.start; 
+    sort(v.begin(), v.end(), [&] (const Plan a, const Plan b) {
+        return a.start < b.start;
     });
     
     vector<string> answer;
     vector<pair<string, int>> st;
-    
     int n = v.size();
-    
     for (int i = 0; i < n; i++) {
         int curStart = v[i].start;
         int curDur = v[i].dur;
-        int nextStart = (i + 1 < n ? v[i+1].start : 1e9);
+        int nextStart = (i+1 < n ? v[i+1].start : 1e9);
         
         st.push_back({v[i].name, curDur});
         
         int gap = nextStart - curStart;
         
         while (!st.empty() && gap > 0) {
-            auto &top = st.back();
-            if (top.second <= gap) {
-                gap -= top.second;
-                answer.push_back(top.first);
+            auto& s = st.back();
+            if (s.second <= gap) {
+                answer.push_back(s.first);
+                gap -= s.second;
                 st.pop_back();
             } else {
-                top.second -= gap;
+                s.second -= gap;
                 gap = 0;
             }
         }
     }
     
     while (!st.empty()) {
-        answer.push_back(st.back().first);
+        auto& s = st.back();
+        answer.push_back(s.first);
         st.pop_back();
-    }
+    }  
+    
     return answer;
 }
