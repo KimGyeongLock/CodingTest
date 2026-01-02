e10ff6bcfee1cdfbf91ceb54d5357899e1c9d7bf
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2026-01-03 02:06:16 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 10.88 ms, Memory: 9.61 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.cpp
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.cpp
@@ -9,32 +9,28 @@ long long solution(int cap, int n, vector<int> deliveries, vector<int> pickups) 
     
     int d = n - 1;
     int p = n - 1;
-
-    while(true) {
-        while(d >= 0 && deliveries[d] == 0) d--;
-        while(p >= 0 && pickups[p] == 0) p--;
+    while (true) {
         
+        while (d >= 0 && deliveries[d] == 0) d--;
+        while (p >= 0 && pickups[p] == 0) p--;
+
         if (d < 0 && p < 0) break;
         
-        int far = max(d, p);
-        answer += (far + 1) * 2;
+        answer += (max(d, p)+1) * 2;
         
-        int capA = cap;
-        int capB = cap;
-        for (int i = d; i >= 0 && capA; i--) {
-            if (deliveries[i] > 0) {
-                int tmp = min(deliveries[i], capA);
-                capA -= tmp;
-                deliveries[i] -= tmp;
-            }
+        int capA = cap, capB = cap;
+        for (int i = d; i >= 0 && capA > 0; i--) {
+            int tmp = min(deliveries[i], capA);
+            deliveries[i] -= tmp;
+            capA -= tmp;
         }
-        for (int i = p; i >= 0 && capB; i--) {
-            if (pickups[i] > 0) {
-                int tmp = min(pickups[i], capB);
-                capB -= tmp;
-                pickups[i] -= tmp;
-            }
+        
+        for (int i = p; i >= 0 && capB > 0; i--) {
+            int tmp = min(pickups[i], capB);
+            pickups[i] -= tmp;
+            capB -= tmp;
         }
     }
+    
     return answer;
 }
