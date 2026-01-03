5286fa7c1a2351a21dd50b2bc53948362c2e133d
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2026-01-04 00:06:13 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 9.98 ms, Memory: 5.86 MB -BaekjoonHub

--- 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.cpp
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.cpp
@@ -1,47 +1,48 @@
 #include <string>
 #include <vector>
-#include <iostream>
 #include <algorithm>
+#include <cmath>
+#include <iostream>
 
 using namespace std;
 
-vector<int> binary(long long numbers) {
-    vector<int> res;
-    while (numbers > 1) {
-        res.push_back(numbers % 2);
-        numbers /= 2;
+string toBinary(long long num) {
+    string res = "";
+    while (num > 0) {
+        res += (num % 2) + '0';
+        num /= 2;
     }
-    res.push_back(numbers);
     reverse(res.begin(), res.end());
-    
-    int len = res.size();
-    int k = 1;
-    while (((1 << k) - 1) < len) k++;
-    int full_len = (1 << k) - 1;
-    res.insert(res.begin(), full_len - len, 0);
     return res;
 }
 
-bool ok(vector<int>& v, int l, int r) {
+bool dfs(string str, int l, int r) {
     if (l == r) return true;
-    int m = (l + r) / 2;
-    if (v[m] == 0) {
-        for (int i = l; i <= r; i++) if (v[i] == 1) return false;
+    
+    int mid = (l + r) / 2;
+    
+    if (str[mid] == '0') {
+        for (int i = l; i <= r; i++) {
+            if (str[i] != '0') return false;
+        }
         return true;
     }
-    
-        return ok(v, l, m - 1) && ok(v, m + 1, r);
-
-} 
+    return dfs(str, l, mid - 1) && dfs(str, mid + 1, r);
+}
 
 vector<int> solution(vector<long long> numbers) {
     vector<int> answer;
-    
     for (long long n : numbers) {
-        vector<int> vec = binary(n);
-
-        int s = vec.size();
-        if (ok(vec, 0, s - 1)) answer.push_back(1);
+        string bin = toBinary(n);
+        int k = 1;
+        while(pow(2, k) - 1 < bin.length()) k++;
+        int padding = pow(2, k) - 1 - bin.length();
+        string nw = "";
+        for (int i = 0; i < padding; i++) nw.push_back('0');
+        for (char b : bin) nw.push_back(b);
+        // cout << nw << "\n";
+        
+        if (dfs(nw, 0, nw.size() - 1)) answer.push_back(1);
         else answer.push_back(0);
     }
     return answer;
