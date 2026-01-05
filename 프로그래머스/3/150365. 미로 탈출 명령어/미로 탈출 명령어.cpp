b241a4d4754da6de408c67980798ede1adb5a13f
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2026-01-05 20:06:24 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 0.04 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.cpp
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.cpp
@@ -4,32 +4,38 @@
 
 using namespace std;
 
-const int dx[4] = {1, 0, 0, -1};   // d, l, r, u
-const int dy[4] = {0, -1, 1,  0};
-const char mv[4] = {'d','l','r','u'};
+const int dx[4] = {1, 0, 0, -1};
+const int dy[4] = {0, -1, 1, 0};
+const char dir[4] = {'d', 'l', 'r', 'u'};
+
+bool isPossible(int n, int m, int x, int y, int r, int c, int k) {
+    if (x < 1 || x > n || y < 1 || y > m) return false;
+    int dist = abs(x - r) + abs(y - c);
+    if (dist > k || (k - dist) % 2 == 1) return false;
+    return true;
+}
 
 string solution(int n, int m, int x, int y, int r, int c, int k) {
     string answer = "";
-    int dist = abs(x - r) + abs(y - c);
-    if (dist > k) return "impossible";
     
-    for (int i = 0; i < k; i++) {
-        int rem = k - i - 1;
-        bool moved = false;
-        for (int j = 0; j < 4; j++) {
-            int nx = x + dx[j];
-            int ny = y + dy[j];
-            if (nx >= 1 && nx <= n && ny >= 1 && ny <= m) {
-                int nd = abs(nx - r) + abs(ny - c);
-                if (nd <= rem) {
-                    answer.push_back(mv[j]);
-                    x = nx; y = ny;
-                    moved = true;
-                    break;
-                }
+    // x,y -> r,c
+    // 불가능한 경우
+    int dist = abs(x - r) + abs(y - c);
+    if (dist > k || (k - dist) % 2 == 1) return "impossible";
+    
+    //
+    int nx = x, ny = y;
+    for (int step = 0; step < k; step++) {
+        for (int i = 0; i < 4; i++) {
+            if (isPossible(n, m, nx + dx[i], ny + dy[i], r, c, k - step - 1)) {
+                nx += dx[i];
+                ny += dy[i];
+                answer.push_back(dir[i]);
+                break;
             }
         }
-        if (!moved) return "impossible";
     }
+    
+           
     return answer;
 }
