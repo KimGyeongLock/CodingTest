057d6e493fdf92831edaff2b6bca7379ebc8be3b
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2026-01-07 00:04:51 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.cpp
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.cpp
@@ -1,31 +1,33 @@
 #include <string>
 #include <vector>
+#include <climits>
 
 using namespace std;
 
-int solution(int storey) {
-    int answer = 0;
+int answer;
+
+void dfs(const vector<int>& digits, int idx, int carry, int cost) {
+    if (cost >= answer) return;
     
-    while (storey > 0) {
-        int d = storey % 10;
-        int next = (storey / 10) % 10;
-        
-        if (d > 5) {
-            answer += 10 - d;
-            storey = storey / 10 + 1;
-        } else if (d < 5) {
-            answer += d;
-            storey = storey / 10;
-        } else {
-            if (next >= 5) {
-                answer += 5;
-                storey = storey / 10 + 1;
-            } else {
-                answer += 5;
-                storey = storey / 10;
-            }
-        }
+    if (idx == digits.size()) {
+        answer = min(answer, cost + carry);
+        return;
     }
     
+    int d = digits[idx] + carry;
+    // 내림
+    dfs(digits, idx + 1, 0, cost + d);
+    // 올림
+    dfs(digits, idx + 1, 1, cost + (10 - d));
+}
+
+int solution(int storey) {
+    vector<int> digits;
+    while (storey > 0) {
+        digits.push_back(storey % 10);
+        storey /= 10;
+    }
+    answer = INT_MAX;
+    dfs(digits, 0, 0, 0);
     return answer;
 }
