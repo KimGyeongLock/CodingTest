643efa966249d4a927857bf9327253ba55df978e
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2026-01-10 21:05:42 +0900

    [level 2] Title: 디펜스 게임, Time: 25.75 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.cpp
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.cpp
@@ -4,23 +4,23 @@
 
 using namespace std;
 
-int solution(int n, int k, vector<int> enemy) {
-    priority_queue<int> pq; 
-    int cleared = 0;
-
-    for (int e : enemy) {
+int solution(int n, int k, vector<int> enemy) {    
+    priority_queue<int> pq;
+    for (int i = 0; i < enemy.size(); i++) {
+        int e = enemy[i];
         n -= e;
         pq.push(e);
-
+        
         if (n < 0) {
-            if (k == 0) break;
-            n += pq.top();
-            pq.pop();
-            k--;
+            if (k > 0) {
+                n += pq.top();
+                pq.pop();
+                k--;
+            } else {
+                return i;
+            }
         }
-
-        cleared++;
+        
     }
-
-    return cleared;
+    return enemy.size();
 }
