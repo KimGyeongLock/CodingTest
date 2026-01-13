6cc82908e70847143dedb8d8ac364eac2b26454f
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2026-01-13 22:26:26 +0900

    [level 2] Title: 귤 고르기, Time: 26.81 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
@@ -1,26 +1,35 @@
-#include <bits/stdc++.h>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <iostream>
+
 using namespace std;
 
 int solution(int k, vector<int> tangerine) {
-    unordered_map<int, int> cnt;
-    cnt.reserve(tangerine.size() * 2);
-
-    // 1) 빈도 세기
-    for (int x : tangerine) cnt[x]++;
-
-    // 2) 빈도만 뽑아서 정렬
-    vector<int> freqs;
-    freqs.reserve(cnt.size());
-    for (auto &p : cnt) freqs.push_back(p.second);
-
-    sort(freqs.begin(), freqs.end(), greater<int>());
-
-    // 3) 큰 것부터 더해서 k개 채우기
-    int sum = 0, kinds = 0;
-    for (int f : freqs) {
-        sum += f;
-        kinds++;
-        if (sum >= k) break;
+    int answer = 0;
+    
+    unordered_map<int, int> mp;
+    for (int t : tangerine) {
+        mp[t]++;
     }
-    return kinds;
+    
+    vector<pair<int, int>> vec;
+    for (auto m : mp) {
+        vec.push_back({m.first, m.second});
+    }
+
+    sort(vec.begin(), vec.end(), [&](const auto a, const auto b) {
+        return a.second > b.second;
+    });
+    
+    for (auto p : vec) {
+        answer++;
+        k -= p.second;
+        if (k <= 0) break;
+    }
+     
+    
+    
+    return answer;
 }
