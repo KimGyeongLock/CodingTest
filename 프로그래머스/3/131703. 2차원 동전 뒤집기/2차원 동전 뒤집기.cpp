00948357c3917f2004b6328a0c931446ccaaed53
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2026-02-16 17:51:37 +0900

    [level 3] Title: 2차원 동전 뒤집기, Time: 0.05 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/3/131703. 2차원 동전 뒤집기/2차원 동전 뒤집기.cpp
+++ 프로그래머스/3/131703. 2차원 동전 뒤집기/2차원 동전 뒤집기.cpp
@@ -7,23 +7,22 @@ using namespace std;
 int solution(vector<vector<int>> beginning, vector<vector<int>> target) {
     int n = beginning.size();
     int m = beginning[0].size();
-    const int INF = INT_MAX;
-    int answer = INF;
+    int answer = INT_MAX;
     
-    for (int mask = 0; mask < (1 << n); mask++) { // 00000 (1, 0으로 오픈 표시) < 100000
+    for (int mask = 0; mask < (1 << n); mask++) {
         int rowFlips = 0;
-        for (int x = mask; x; x >>= 1) rowFlips += (x & 1); // 101 -> 10 -> 1 (1의 갯수 = 3)
+        for (int x = mask; x; x >>= 1) if (x & 1) rowFlips++;
         
         int colFlips = 0;
         bool ok = true;
         
-        for (int c = 0; c < m && ok; c++) { //열 검사
+        for (int c = 0; c < m && ok; c++) {
             bool allSame = true;
             bool allOpp = true;
             
             for (int r = 0; r < n; r++) {
                 int cur = beginning[r][c];
-                if (mask & (1 << r)) cur ^= 1; // (1 << 0) => 0001 (1 << 1) => 0010 (1 << 2) 0100 (XOR) 
+                if (mask & (1 << r)) cur ^= 1;
                 
                 if (cur != target[r][c]) allSame = false;
                 if ((cur ^ 1) != target[r][c]) allOpp = false;
@@ -34,15 +33,15 @@ int solution(vector<vector<int>> beginning, vector<vector<int>> target) {
             } else if (allOpp) {
                 colFlips++;
             } else {
-                ok = false; // 섞여 있음
+                ok = false;
             }
         }
         
         if (ok) {
             int total = rowFlips + colFlips;
-            if (total < answer) answer = total;
+            answer = min(answer, total);
         }
     }
     
-    return answer == INF ? -1 : answer;
+    return answer == INT_MAX ? -1 : answer;
 }
