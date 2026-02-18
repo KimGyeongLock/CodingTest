debd8211f5eeefde7f6af17d1c4c7c07fa2aa2ef
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2026-02-18 22:32:13 +0900

    [level 3] Title: 고고학 최고의 발견, Time: 63.05 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/3/131702. 고고학 최고의 발견/고고학 최고의 발견.cpp
+++ 프로그래머스/3/131702. 고고학 최고의 발견/고고학 최고의 발견.cpp
@@ -8,11 +8,13 @@ int dx[5] = {0, 1, 0, -1, 0};
 int dy[5] = {0, 0, 1, 0, -1};
 
 void rotateCell(vector<vector<int>>& a, int x, int y, int k) {
-    int n = a.size();
-    for (int dir = 0; dir < 5; dir++) {
-        int nx = x + dx[dir];
-        int ny = y + dy[dir];
-        if (0 <= nx && nx < n && 0 <= ny && ny < n) {
+    int n = a.size(); 
+    
+    for (int i = 0; i < 5; i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        
+        if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
             a[nx][ny] = (a[nx][ny] + k) % 4;
         }
     }
@@ -24,27 +26,21 @@ int solution(vector<vector<int>> clockHands) {
     
     int total = 1;
     for (int i = 0; i < n; i++) total *= 4;
-    
     for (int mask = 0; mask < total; mask++) {
         auto a = clockHands;
         int cnt = 0;
         
         int tmp = mask;
-        for (int j = 0; j < n; j++) {
-            int k = tmp % 4; // k = 0~3
+        for (int r = 0; r < n; r++) {
+            int k = tmp % 4;
             tmp /= 4;
-            if (k) rotateCell(a, 0, j, k);
+            if (k) rotateCell(a, 0, r, k);
             cnt += k;
         }
         
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                int upper = a[i - 1][j]; // upper => 0
-                // (upper + k) % 4 = 0
-                // upper + k ≡ 0 (mod 4)
-                // k ≡ -upper (mod 4)
-                // -upper ≡ 4 + (-upper)
-                // k ≡ (4 - upper) % 4
+                int upper = a[i - 1][j];
                 int k = (4 - upper) % 4;
                 if (k) rotateCell(a, i, j, k);
                 cnt += k;
@@ -52,13 +48,18 @@ int solution(vector<vector<int>> clockHands) {
         }
         
         bool ok = true;
-        for (int j = 0; j < n; j++) {
-            if (a[n - 1][j] != 0) {
+        for (int i = 0; i < n; i++) {
+            if (a[n - 1][i] != 0) {
                 ok = false;
                 break;
             }
         }
-        if (ok) best = min(best, cnt);
+        
+        if (ok) {
+            best = min(best, cnt);   
+        }
     }
+    
+    
     return best;
 }
