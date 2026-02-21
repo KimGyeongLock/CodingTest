80f1df84ea032cf8f048b37326a85bea130eb7b6
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2026-02-21 11:30:25 +0900

    [level 3] Title: 등산코스 정하기, Time: 60.27 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.cpp
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.cpp
@@ -1,50 +1,54 @@
 #include <string>
 #include <vector>
 #include <queue>
-#include <algorithm>
 #include <climits>
+#include <algorithm>
 
 using namespace std;
 
 vector<int> solution(int n, vector<vector<int>> paths, vector<int> gates, vector<int> summits) {    
-    vector<vector<pair<int, int>>> edge(n + 1);
-    for (auto& p : paths) {
-        edge[p[0]].push_back({p[1], p[2]});
-        edge[p[1]].push_back({p[0], p[2]});
+    vector<vector<pair<int, int>>> edgeList(n+1);
+    for (auto p : paths) {
+        edgeList[p[0]].push_back({p[1], p[2]});
+        edgeList[p[1]].push_back({p[0], p[2]});
     }
     
+    vector<bool> isSummit(n+1, false);
+    for (int s : summits) isSummit[s] = true;
+    
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
-    vector<int> dist(n + 1, INT_MAX);
-    vector<char> isGate(n + 1, 0), isSummit(n + 1, 0);
-    for (int g : gates) isGate[g] = 1;
-    for (int s : summits) isSummit[s] = 1;
-    for (int g : gates) {
-        dist[g] = 0;
-        pq.push({0, g});
+    vector<int> intensity(n+1, INT_MAX);
+    
+    for (int gate : gates) {
+        intensity[gate] = 0;
+        pq.push({0, gate});
     }
     
     while(!pq.empty()) {
-        auto [d, u] = pq.top(); pq.pop();
-        if (d != dist[u]) continue;
-        if (isSummit[u]) continue;
-        for (auto [v, w] : edge[u]) {
-            if (isGate[v]) continue;
-            int nd = max(d, w);
-            if (nd < dist[v]) {
-                dist[v] = nd;
-                pq.push({nd, v});
+        auto [curIntensity, cur] = pq.top(); pq.pop();
+        
+        if (curIntensity > intensity[cur]) continue;
+        if (isSummit[cur]) continue;
+        
+        for (auto [next, w] : edgeList[cur]) {
+            int maxIntensity = max(w, curIntensity);
+            if (intensity[next] > maxIntensity) {
+                intensity[next] = maxIntensity;
+                pq.push({intensity[next], next});
             }
         }
     }
     
     sort(summits.begin(), summits.end());
-    int best1, best2 = INT_MAX;
+    
+    int bestIntensity = INT_MAX;
+    int bestNode = 0;
     for (int s : summits) {
-        if (dist[s] < best2) {
-            best2 = dist[s];
-            best1 = s;
+        if (intensity[s] < bestIntensity) {
+            bestIntensity = intensity[s];
+            bestNode = s;
         }
     }
     
-    return {best1, best2};
+    return {bestNode, bestIntensity};
 }
