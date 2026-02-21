89b1eef87b2b3a4028cff6256c9da1f982111f9f
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2026-02-21 15:59:18 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 6.59 ms, Memory: 26.8 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cpp
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cpp
@@ -6,41 +6,33 @@ using namespace std;
 
 int solution(vector<int> queue1, vector<int> queue2) {
     int answer = 0;
-    long sum = 0, sum1 = 0;
-    if (sum % 2 == 1) return -1;
-    
+    int n = queue1.size();
     queue<int> q1, q2;
+    long t1 = 0, t2 = 0;
     for (int i : queue1) {
-        sum += i;
-        sum1 += i;
         q1.push(i);
+        t1 += i;
     }
-    for (int j : queue2) {
-        sum += j;
-        q2.push(j);
-    }
-    long half = sum / 2;
-    for (int i : queue1) {
-        if (half < i) return -1;
-    }
-    for (int j : queue2) {
-        if (half < j) return -1;
-    }
-    while (sum1 != half) {
-        answer += 1;
-        if (answer >= 3*(queue1.size() + queue2.size())) return -1;
-        if (sum1 > half) {
-            int f = q1.front();
-            q1.pop();
-            q2.push(f);
-            sum1 -= f;
-        } else if (sum1 < half) {
-            int f = q2.front();
-            q2.pop();
-            q1.push(f);
-            sum1 += f;
-        }
+    for (int i : queue2) {
+        q2.push(i);
+        t2 += i;
     }
     
+    while (t1 != t2) {
+        answer++;
+        if (t1 > t2) {
+            t1 -= q1.front();
+            t2 += q1.front();
+            q2.push(q1.front());
+            q1.pop();
+        }
+        else {
+            t2 -= q2.front();
+            t1 += q2.front();
+            q1.push(q2.front());
+            q2.pop();
+        }
+        if (answer > 3 * n) return -1;
+    }
     return answer;
 }
