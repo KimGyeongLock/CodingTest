bace9fbb82ab3147a416827db4761f9509303a6d
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2026-02-23 19:26:00 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 26.99 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.cpp
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.cpp
@@ -1,6 +1,5 @@
 #include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
 
@@ -8,9 +7,9 @@ int solution(vector<vector<int>> board, vector<vector<int>> skill) {
     int n = board.size();
     int m = board[0].size();
     
-    vector<vector<int>> diff(n+2, vector<int>(m+2, 0));
-     
-    for (auto s : skill) {
+    vector<vector<int>> diff(n + 2, vector<int>(m + 2, 0));
+    
+    for (auto& s : skill) {
         int type = s[0], r1 = s[1], c1 = s[2], r2 = s[3], c2 = s[4];
         int degree = (type == 1) ? -s[5] : s[5];
         
@@ -32,12 +31,12 @@ int solution(vector<vector<int>> board, vector<vector<int>> skill) {
         }
     }
     
-    int ans = 0;
+    int answer = 0;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            if(board[i][j] + diff[i][j] > 0) ans++;
+            if (board[i][j] + diff[i][j] > 0) answer++;
         }
     }
     
-    return ans;
+    return answer;
 }
