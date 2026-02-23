471598e387c497e96fa80847c6c9af969e13ffd8
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2026-02-23 15:06:15 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 36.53 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.cpp
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.cpp
@@ -1,50 +1,43 @@
 #include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
 int solution(vector<vector<int>> board, vector<vector<int>> skill) {
     int n = board.size();
     int m = board[0].size();
-    vector<vector<int>> diff(n + 1, vector<int>(m + 1, 0));
-        
-    for (auto& s : skill) {
-        int type = s[0];
-        int r1 = s[1];
-        int c1 = s[2];
-        int r2 = s[3];
-        int c2 = s[4];
-        int degree = s[5];
-        
-        if (type == 1) degree = -degree;
+    
+    vector<vector<int>> diff(n+2, vector<int>(m+2, 0));
+     
+    for (auto s : skill) {
+        int type = s[0], r1 = s[1], c1 = s[2], r2 = s[3], c2 = s[4];
+        int degree = (type == 1) ? -s[5] : s[5];
         
         diff[r1][c1] += degree;
-        diff[r1][c2+1] -= degree;
+        diff[r1][c2 + 1] -= degree;
         diff[r2 + 1][c1] -= degree;
         diff[r2 + 1][c2 + 1] += degree;
     }
     
-    for (int i = 0; i < n; i++) {
-        int run = 0;
-        for (int j = 0; j < m; j++) {
-            run += diff[i][j];
-            diff[i][j] = run;
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= m; j++) {
+            diff[i + 1][j] += diff[i][j];
         }
     }
     
-    for (int j = 0; j < m; j++) {
-        int run = 0;
-        for (int i = 0; i < n; i++) {
-            run += diff[i][j];
-            diff[i][j] = run;
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= m; j++) {
+            diff[i][j + 1] += diff[i][j];
         }
     }
     
-    int cnt = 0;
+    int ans = 0;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            if (board[i][j] + diff[i][j] >= 1) cnt++;
+            if(board[i][j] + diff[i][j] > 0) ans++;
         }
     }
-    return cnt;
+    
+    return ans;
 }
