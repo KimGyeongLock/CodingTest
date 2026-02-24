add8950ad7b33269c6c56d57a5af0a2941d3ce11
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2026-02-24 17:09:02 +0900

    [level 3] Title: 양과 늑대, Time: 1.00 ms, Memory: 3.72 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
@@ -2,35 +2,40 @@
 #include <vector>
 
 using namespace std;
-int answer = 0;
 
-void dfs(int sheep, int wolf, vector<int> cand, const vector<int>& info, const vector<vector<int>>& edges) {
-    answer = max(answer, sheep);
-    for (int i = 0; i < cand.size(); i++) {
-        int u = cand[i];
-        int nsheep = sheep + (info[u] == 0);
-        int nwolf = wolf + (info[u] == 1);
-        if (nwolf >= nsheep) continue;
+vector<vector<int>> edgeList;
+vector<int> visited;
+int maxSheep = 0;
+
+void dfs(vector<int>& info, int sheep, int wolf, vector<int> possible) {
+    maxSheep = max(maxSheep, sheep);
+    for (int i = 0; i < possible.size(); i++) {
+        vector<int> tmp = possible;
+        int nsheep = sheep, nwolf = wolf;
+        int p = tmp[i];
         
-        vector<int> next = cand;
-        next.erase(next.begin() + i);
-        
-        for (int c : edges[u]) {
-            next.push_back(c);
+        if (info[p] == 0) { // sheep
+            nsheep++;
+        } else { // wolf
+            nwolf++;
+            if (nwolf >= nsheep) continue;
         }
-        
-        dfs(nsheep, nwolf, next, info, edges);
-    }
+        tmp.erase(tmp.begin() + i);
+        for (auto& e : edgeList[p]) tmp.push_back(e);
+        visited[p] = true;
+        dfs(info, nsheep, nwolf, tmp);
+        visited[p] = false;
+    }   
 }
 
 int solution(vector<int> info, vector<vector<int>> edges) {
-    
-    vector<vector<int>> edgeList(info.size());
+    int n = info.size();
+    edgeList.assign(n, {});
+    visited.assign(n, false);
     for (auto& e : edges) {
         edgeList[e[0]].push_back(e[1]);
     }
-    
-    vector<int> cand = {0};
-    dfs(0, 0, cand, info, edgeList);
-    return answer;
+    visited[0] = true;
+    dfs(info, 1, 0, edgeList[0]);
+    return maxSheep;
 }
