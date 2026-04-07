8bf99c887e4ccfcaf7141acc3261a5e98dbf2cf8
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2026-04-07 17:05:09 +0900

    [level 2] Title: 전화번호 목록, Time: 348.26 ms, Memory: 98.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,10 +2,10 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Set<String> set = new HashSet<>(Arrays.asList(phone_book));
-        for (String num : phone_book) {
-            for (int cut = 1; cut < num.length(); cut++) {
-                if (set.contains(num.substring(0, cut))) return false;
+        Arrays.sort(phone_book);
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) {
+                return false;
             }
         }
         return true;
