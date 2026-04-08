ceb2186459806e5b1557c8ae6103451c35497464
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2026-04-08 17:23:10 +0900

    [level 2] Title: 소수 찾기, Time: 33.06 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
 
 ### 성능 요약
 
-메모리: 96.4 MB, 시간: 22.31 ms
+메모리: 82.9 MB, 시간: 33.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 13일 02:25:26
+2026년 04월 08일 17:23:08
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,38 +1,34 @@
 import java.util.*;
 class Solution {
-    HashSet<Integer> set = new HashSet<>(); 
-    boolean[] visited;
+    HashSet<Integer> set = new HashSet<>();
     public int solution(String numbers) {
+        boolean[] visited = new boolean[numbers.length()];
+        dfs("", numbers, visited);
+       
         int answer = 0;
-        visited = new boolean[numbers.length()];
-        generateNumbers(numbers, "");
-
-        for(int s : set) {
-            if (isPrime(s)) {
-                answer++;                
-            }
+        for (int s : set) {
+            if (isPrime(s)) answer++;
         }
         return answer;
     }
     
-    private void generateNumbers(String numbers, String current) {
-        if (current != "") {
+    private void dfs(String current, String numbers, boolean[] visited) {
+        if (!current.equals("")) {
             set.add(Integer.parseInt(current));
         }
-        
-        for(int i = 0; i < numbers.length(); i++) {
-            if (!visited[i]) {
+        for (int i = 0; i < numbers.length(); i++) {
+            if(!visited[i]) {
                 visited[i] = true;
-                generateNumbers(numbers, current + numbers.charAt(i));
+                dfs(current + numbers.charAt(i), numbers, visited);
                 visited[i] = false;
             }
         }
     }
     
-    private boolean isPrime(int a) {
-        if (a < 2) return false;
-        for(int i = 2; i <= Math.sqrt(a); i++) {
-            if (a % i == 0) return false;
+    private boolean isPrime(int x) {
+        if (x < 2) return false;
+        for (int i = 2; i * i <= x; i++) {
+            if (x % i == 0) return false;
         }
         return true;
     }
