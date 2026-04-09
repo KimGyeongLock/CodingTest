24f7f8daeeacb5fe43355e732701e98ba0ef82e4
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2026-04-09 17:42:41 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 6.41 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,45 +1,38 @@
 import java.util.*;
 
 class Solution {
-    private boolean[][] graph;
     public int solution(int n, int[][] wires) {
-        graph = new boolean[n + 1][n + 1];
-        for(int[] w : wires) {
-            graph[w[0]][w[1]] = graph[w[1]][w[0]] = true;
+        int answer = Integer.MAX_VALUE;
+        int s = wires.length;
+        
+        for (int i = 0; i < s; i++) {
+            ArrayList<ArrayList<Integer>> edgeList = new ArrayList<>();
+            
+            for (int j = 0; j <= n; j++) {
+                edgeList.add(new ArrayList<>());
+            }
+            
+            for (int j = 0; j < s; j++) {
+                if (j == i) continue; 
+                edgeList.get(wires[j][0]).add(wires[j][1]);
+                edgeList.get(wires[j][1]).add(wires[j][0]);
+            }
+            
+            boolean[] visited = new boolean[n + 1];
+            int cnt = dfs(1, visited, edgeList);
+            
+            answer = Math.min(answer, Math.abs(cnt - n + cnt));
         }
         
-        int answer = Integer.MAX_VALUE;
-        for(int[] w : wires) {
-            int a = w[0];
-            int b = w[1];
-            graph[a][b] = graph[b][a] = false;
-            
-            int cnt = bfs(n, a);
-            int diff = Math.abs(n - cnt - cnt);
-            answer = Math.min(answer, diff);
-            
-            graph[a][b] = graph[b][a] = true;
-            
-        }
         return answer;
     }
     
-    private int bfs(int n, int start) {
-        boolean[] visited = new boolean[n + 1];
-        Queue<Integer> q = new LinkedList<>();
-        visited[start] = true;
-        q.offer(start);
+    private int dfs(int start, boolean[] visited, ArrayList<ArrayList<Integer>> edgeList) {
         int cnt = 1;
-        
-        while(!q.isEmpty()) {
-            int current = q.poll();
-            for(int i = 1; i <= n; i++) {
-                if(graph[current][i] && !visited[i]) {
-                    q.offer(i);
-                    visited[i] = true;
-                    cnt++;
-                }
-            }
+        visited[start] = true;
+        for (int nxt : edgeList.get(start)) {
+            if (visited[nxt]) continue;
+            cnt += dfs(nxt, visited, edgeList);
         }
         return cnt;
     }
