2c21eb175698023c5345dff4984679e2eaae0133
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2026-05-08 17:10:37 +0900

    [level 2] Title: 전화번호 목록, Time: 61.53 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=cpp) 
 
 ### 성능 요약
 
-메모리: 98.7 MB, 시간: 348.26 ms
+메모리: 31.4 MB, 시간: 61.53 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 07일 17:05:06
+2026년 05월 08일 17:10:34
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,24 +1,14 @@
 #include <string>
 #include <vector>
-#include <unordered_set>
+#include <algorithm>
 
 using namespace std;
 
 bool solution(vector<string> phone_book) {
-    unordered_set<string> s;
-    for(string phone : phone_book) {
-        s.insert(phone);
+    bool answer = true;
+    sort(phone_book.begin(), phone_book.end());
+    for (int i = 0; i < phone_book.size() - 1; i++) {
+        if (phone_book[i+1].find(phone_book[i]) == 0) answer = false;
     }
-    
-    for(string phone : phone_book) {
-        string temp = "";
-        for(char c : phone) {
-            temp += c;
-            if (s.find(temp) != s.end() && temp != phone) {
-                return false;
-            }
-        }
-    }
-    
-    return true;
+    return answer;
 }
