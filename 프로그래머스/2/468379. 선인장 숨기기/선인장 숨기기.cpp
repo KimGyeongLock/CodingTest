c477e0a214e2d2fc5e794a37c2c6644f6ff38a20
GyeongLockKim <63464299+KimGyeongLock@users.noreply.github.com>
2026-06-02 19:03:07 +0900

    [level 2] Title: 선인장 숨기기, Time: 106.39 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/2/468379. 선인장 숨기기/선인장 숨기기.cpp
+++ 프로그래머스/2/468379. 선인장 숨기기/선인장 숨기기.cpp
@@ -1,70 +1,56 @@
 #include <string>
 #include <vector>
-#include <deque>
+#include <climits>
+#include <queue>
 
 using namespace std;
 
-vector<int> solution(int m, int n, int h, int w, vector<vector<int>> drops) {    
-    int INF = drops.size() + 1;
-    vector<vector<int>> board(m, vector<int>(n, INF));
-    
+vector<int> solution(int m, int n, int h, int w, vector<vector<int>> drops) {
+    vector<vector<int>> board(m, vector<int>(n, INT_MAX));
     for (int i = 0; i < drops.size(); i++) {
         board[drops[i][0]][drops[i][1]] = i + 1;
     }
     
     int colCount = n - w + 1;
-    vector<vector<int>> rowMin(m, vector<int>(colCount));
-    
+    vector<vector<int>> rowMin(m, vector<int>(colCount, INT_MAX));
     for (int i = 0; i < m; i++) {
-        deque<int> dq; // dq.front()는 항상 현재 구간의 최솟값 위치
-        
+        deque<int> dq;
         for (int j = 0; j < n; j++) {
             while (!dq.empty() && board[i][dq.back()] >= board[i][j]) {
-                dq.pop_back(); 
-                // 새로 들어온 것(board[i][j])이 기존에 있던 값(board[i][dq.back()])보다 크거나 같으면 기존 값 제거
+                dq.pop_back();
             }
             dq.push_back(j);
-            
             if (dq.front() <= j - w) {
                 dq.pop_front();
             }
-            
-            if (j >= w - 1) { // w가 채워졌다
+            if (j >= w - 1) {
                 rowMin[i][j - w + 1] = board[i][dq.front()];
             }
         }
     }
     
-    int bestValue = -1;
     vector<int> answer = {0, 0};
-    
+    int bestValue = -1;
     for (int j = 0; j < colCount; j++) {
         deque<int> dq;
-        
         for (int i = 0; i < m; i++) {
-            while(!dq.empty() && rowMin[dq.back()][j] >= rowMin[i][j]) {
+            while (!dq.empty() && rowMin[dq.back()][j] >= rowMin[i][j]) {
                 dq.pop_back();
             }
-            
             dq.push_back(i);
-            
             if (dq.front() <= i - h) {
                 dq.pop_front();
             }
-            
             if (i >= h - 1) {
-                int value = rowMin[dq.front()][j];
                 int top = i - h + 1;
-                if (value > bestValue ||
-                   (value == bestValue && (top < answer[0] || (top == answer[0] && j < answer[1])))) {
+                int value = rowMin[dq.front()][j];
+                if (bestValue < value ||
+                    bestValue == value && (top < answer[0] || (top == answer[0] && j < answer[1]))) {
                     bestValue = value;
                     answer = {top, j};
                 }
             }
-            
         }
     }
-    
-    
     return answer;
 }
